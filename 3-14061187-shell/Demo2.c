b9dd35649175ef369a9d2fbf7425f0ace26d957e
lb1100 <436101042@qq.com>
2016-03-18 23:07:30 +0800

    修复ctrl+z/c BUG

--- 3-14061187-shell/Demo2.c
+++ 3-14061187-shell/Demo2.c
@@ -3,10 +3,10 @@
 int main(){
 	int i = 0;
 	printf("Demo is running\n");
-	while(i<20){
+	while(i<100){
 		i=i+1;
 		sleep(1);
-		printf("Demo has running %d seconds.\n", i);
+		printf(" ^-Demo has running %d seconds.\n", i);
 		fflush(stdout);
 	}
 	printf("Demo is ending.\n");
